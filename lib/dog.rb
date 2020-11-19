339223e2601fb706c8c5eb252ce388fcccde0a01
Zach <zbayard@gmail.com>
2020-11-19 15:31:44 -0500

    finished

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name 
+
+  @@all = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name 
+    @owner = owner 
+    @mood = mood
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,67 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name, species = "human")
+    @name = name
+    @species = species
+    @@all << self
+
+  end 
+
+  def say_species 
+    return "I am a #{@species}." 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.count 
+    return @@all.length 
+  end
+  
+  def self.reset_all
+    @@all.clear 
+  end
+
+  def cats
+    Cat.all.select {|cats| cats.owner==self}
+  end
+
+  def dogs 
+    Dog.all.select {|dogs| dogs.owner==self}
+  end 
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dogs
+    dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    cats.each {|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    pets = dogs + cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets 
+    return "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+
+  end 
+
+
 end
